9196ee8cb91c26031c3e00632c6017144116c36a
Sophie Mann <sklm@live.co.uk>
2017-05-11 20:51:55 +0100

    adds jquery to implement thermostat functionality

---
+++ index.js
@@ -0,0 +1,52 @@
+$( document ).ready(function() {
+  var thermostat = new Thermostat();
+   updateTemperature();
+   displayWeather('London');
+   $( '#psm-mode' ).text('On');
+
+   $('#increase-temp').click(function(){
+     thermostat.increase();
+     updateTemperature();
+   });
+
+   $('#decrease-temp').click(function() {
+     thermostat.decrease();
+     updateTemperature();
+
+   });
+
+   $('#reset').click(function(){
+     thermostat.resetTemperature();
+     updateTemperature();
+   });
+
+   $('#psm-on').click(function(){
+     thermostat.switchPSMOn();
+     $( '#psm-mode' ).text('On');
+
+   });
+
+   $('#psm-off').click(function(){
+     thermostat.switchPSMOff();
+     $( '#psm-mode' ).text('Off');
+   });
+
+   $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city);
+   });
+//
+   function updateTemperature(){
+    $( '#temperature' ).text(thermostat.getTemperature());
+    $( '#energy-usage' ).text(thermostat.energyUsage());
+    $('#energy-usage').attr('class', thermostat.energyUsage());
+   }
+
+   function displayWeather(city) {
+   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=188d4bfce0208cc4d0252a7dc677a8f6&units=metric', function(data) {
+     $('#current-temperature').text(data.main.temp);
+   });
+
+   }
+
+});
