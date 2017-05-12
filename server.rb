26c52493ebc905f14fc0f34abff22e643a33fd3d
Sophie Mann <sklm@live.co.uk>
2017-05-12 18:55:01 +0100

    updates routes

--- server.rb
+++ server.rb
@@ -4,11 +4,9 @@ set :public_folder, proc { File.join(root)}
 
 get '/temperature' do
   headers 'Access-Control-Allow-Origin' => '*'
-  $saved_temperature
 end
 
 
 post '/temperature' do
   headers 'Access-Control-Allow-Origin' => '*'
-  $saved_temperature = params[:saved_temperature]
 end
